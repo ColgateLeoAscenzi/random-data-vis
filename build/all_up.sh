1678e38ddbb55fc6abb557909c9c57b9233c1ced
Leo Ascenzi <leo.ascenzi@gmail.com>
2025-12-07 22:01:24 -0500

    testing new nginx

--- build/all_up.sh
+++ build/all_up.sh
@@ -3,7 +3,7 @@ docker compose -f "../deployments/docker-compose.yml" build broker
 docker compose -f "../deployments/docker-compose.yml" build generatedata
 docker compose -f "../deployments/docker-compose.yml" build nginx
 echo "Starting Kafka broker container and then waiting 3 seconds"
-docker compose -f "../deployments/docker-compose.yml" up -d
+docker compose -f "../deployments/docker-compose.yml" up -d broker generatedata
 sleep 2
 echo "Kafka started, creating trade topics"
 docker exec -it broker /opt/kafka/bin/kafka-topics.sh --create --topic trade-bids --bootstrap-server broker:29092
@@ -13,3 +13,5 @@ sleep 3
 
 docker build -t consumer:latest ../server/
 docker run -d --network backend --name consumer consumer
+sleep 3
+docker compose -f "../deployments/docker-compose.yml" up -d nginx
